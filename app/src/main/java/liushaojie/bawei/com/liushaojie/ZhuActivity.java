86db6aa91d6fbc8833488633a2d2daa20db6a3b9
lsj123321 <1916530129@qq.com>
2019-02-13 16:44:47 +0800

    first commit

---
+++ app/src/main/java/liushaojie/bawei/com/liushaojie/ZhuActivity.java
@@ -0,0 +1,13 @@
+package liushaojie.bawei.com.liushaojie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ZhuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zhu);
+    }
+}
